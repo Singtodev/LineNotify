c9bc151d1518ad660abe775adf090a7a60c9167f
Singtodev <138797225+Unkeloc4@users.noreply.github.com>
2023-07-31 19:05:08 +0700

    Initial commit

---
+++ lineNotify.py
@@ -0,0 +1,67 @@
+import requests
+import os;
+class LineNotify:
+    
+    def __init__(self):
+
+        current_dir = os.path.dirname(os.path.abspath(__file__));
+
+        file_path = current_dir + "/config.txt"
+
+        try:
+            with open(file_path, "r") as file:
+                for line in file:
+                    str = line.strip().split("=");
+                    if str[0] == "line_token":
+                        self.line_token = str[1];
+                    elif str[0] == "line_api_url":
+                        self.line_api_url = str[1];
+        
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+
+    def sendNotifyMessage(self , message):
+
+
+
+
+        headers = {
+            "Authorization": f"Bearer " + self.line_token,
+        }
+
+        data = {
+            "message": message,
+        }
+
+
+        response = requests.post(self.line_api_url, headers=headers , data=data);
+
+        if response.status_code == 200:
+            print("Message Line Send!");
+        else:
+            print(F"Failed To send Message. Response : {response.status_code} , {response.text}");
+
+    def sendNotifyStickerAndMessage(self , message , sticker_package_id , sticker_id):
+
+        headers = {
+            "Authorization": f"Bearer " + self.line_token,
+        }
+
+        data = {
+            "message": message,
+            "stickerPackageId": sticker_package_id,
+            "stickerId": sticker_id,
+        }
+
+        response = requests.post(self.line_api_url, headers=headers , data=data);
+
+        if response.status_code == 200:
+            print("Send Message And Sticker Success!");
+        else:
+            print(F"Failed To send Sticker Line And Message. Response : {response.status_code} , {response.text}");
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,20 @@
+from lineNotify import *;
+
+
+class Main ():
+
+    def __init__(self):
+        self.startProcess();
+
+    def startProcess(self):
+
+        ln = LineNotify()
+
+        ## sticker package id 1 sticker_id 19 Angry Brown
+        ## sticker package id 2 sticker_id 26 Coney Sleep
+        ## sticker package id 2 sticker_id 26 Coney Fighting
+        
+        ln.sendNotifyMessage("Hello World This is a first Message");
+        ln.sendNotifyStickerAndMessage("Cony Go Sleep" , 2 , 26);
+
+start = Main();
